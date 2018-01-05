9c31ec7a0ec5422bee4bc19fcd989b8d899f921e
Adam Calhoun <a.t.calhoun@gmail.com>
2018-01-05 10:00:51 -0800

    fails getGuests test

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -1,3 +1,11 @@
 public class EventPlanner {
+  private int guests;
 
+  EventPlanner(){
+    guests = -2;
+  }
+
+  public int getGuests(){
+    return guests;
+  }
 }
--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -4,8 +4,15 @@ import static org.junit.Assert.*;
 
 public class EventPlannerTest {
   @Test
-  public void test(){
+  public void eventPlanner_instantiatesCorrectly_bool(){
     EventPlanner testEvent = new EventPlanner();
     assertEquals(true, testEvent instanceof EventPlanner);
+  }
+
+  @Test
+  public void getGuests_getsNumberOfGuests_bool(){
+    EventPlanner testEvent = new EventPlanner();
+    int output = testEvent.getGuests();
+    assert(output > -1);
   }
 }
