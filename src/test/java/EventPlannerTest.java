902ece01f0dba175731788e6aecb0e1bd8561b8f
Adam Calhoun <a.t.calhoun@gmail.com>
2018-01-05 10:09:00 -0800

    refactor tests to all use the same instance of testEvent

--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -3,16 +3,26 @@ import static org.junit.Assert.*;
 
 
 public class EventPlannerTest {
+  EventPlanner testEvent = new EventPlanner();
+  int guests = testEvent.getGuests();
+
   @Test
   public void eventPlanner_instantiatesCorrectly_bool(){
-    EventPlanner testEvent = new EventPlanner();
+//    EventPlanner testEvent = new EventPlanner();
     assertEquals(true, testEvent instanceof EventPlanner);
   }
 
   @Test
-  public void getGuests_getsNumberOfGuests_bool(){
-    EventPlanner testEvent = new EventPlanner();
-    int output = testEvent.getGuests();
-    assert(output > -1);
+  public void getGuests_getsNumberOfGuests_int(){
+//    EventPlanner testEvent = new EventPlanner();
+//    int output = testEvent.getGuests();
+    assert(guests > -1);
   }
+
+//  @Test
+//  public void getFood_getFoodOffered_int(){
+////    EventPlanner testEvent = new EventPlanner();
+//    int output = testEvent.getFood.size();
+//
+//  }
 }
